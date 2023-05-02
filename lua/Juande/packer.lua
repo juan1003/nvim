bffdf5814c2cdff847246ec82b0a3f45db3fb2e6
Juan De Jesus <jdejesus93@gmail.com>
2023-05-02 12:54:39 -0400

    Installing Neotree

--- lua/Juande/packer.lua
+++ lua/Juande/packer.lua
@@ -54,4 +54,16 @@ use('nvim-treesitter/playground')
 use('theprimeagen/harpoon')
 use('mbbill/undotree')
 use('tpope/vim-fugitive')
+-- Unless you are still migrating, remove the deprecated commands from v1.x
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+use {
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
 end)
